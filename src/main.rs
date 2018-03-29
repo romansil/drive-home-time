19920a624cfba21e12a981751ee413220d09b651
rs <rs@hack.more.systems>
2018-03-29 20:44:19 +0200

    command line arguments

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,50 @@
 extern crate reqwest;
 
 use reqwest::Url;
+use std::env;
 
 const URL: &'static str = "https://maps.googleapis.com/maps/api/distancematrix/json";
 //const URL: &'static str = "http://httpbin.org/get";
 
+
+#[derive(Debug)]
+struct Way {
+    src: String,
+    dst: String,
+}
+
+impl Way {
+    fn new(args: &[String]) -> Way {
+        let src = args[1].clone();
+        let dst = args[2].clone();
+
+        Way { src, dst }
+    }
+
+    fn get_time(self) {
+
+        let params = [
+            ("units", "metric"),
+            ("origins", self.src.as_str()),
+            ("destinations", self.dst.as_str()),
+            //("key", option_env!("API_KEY").unwrap())
+        ];
+
+        let url = Url::parse_with_params(URL, &params).unwrap();
+        let res = reqwest::get(url).unwrap().text().unwrap();
+        println!("res: {}", res);
+    }
+}
+
+
 fn main() {
 
-    let params = [
-        ("units", "metric"),
-        ("origins", "New York"),
-        ("destinations", "Washington DC"),
-        //("key", option_env!("API_KEY").unwrap())
-    ];
+    let args: Vec<String> = env::args().collect();
 
-    let url = Url::parse_with_params(URL, &params).unwrap();
+    if args.len() != 3 {
+        panic!("Usage: drive-home-time source destination");
+    }
 
-    let res = reqwest::get(url).unwrap().text().unwrap();
-    println!("res: {}", res);
+    let way = Way::new(&args);
+    way.get_time();
 }
